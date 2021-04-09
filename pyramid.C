1e4682e2ea045160f2b9f3bbeede3bc3fc418031
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-04-09 07:44:08 +0530

    Pyramid Pattern programming with output
    Output :- 
                                           1                                                                                       
                                    1               2                                                                               
                            1               2               3                                                                       
                    1               2               3               4                                                               
            1               2               3               4               5                                                       
                    1               2               3               4                                                               
                            1               2               3                                                                       
                                    1               2                                                                               
                                            1

---
+++ pyramid.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int j;
+    int k;
+    
+    for(i = 1; i <= 5; i++)
+    {
+        for(k = 5; k >= i; k--)
+        {
+            printf("\t");
+        }
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d\t\t", j);
+        }
+        printf("\n");
+    }
+    
+    for(i = 4; i >= 1; i--)
+    {
+        for(k = 5; k >= i; k--)
+        {
+            printf("\t");
+        }
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d\t\t", j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
